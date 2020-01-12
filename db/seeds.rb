8e1b407785b095a629649596a5bb3ca04b0ede1a
moseslucas <mo@moseslucas.com>
2020-01-12 16:41:20 +0800

    Scaffolds Game title description price

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Game.create([
+  {title: 'Death Stranding', price: 3000},
+  {title: 'Red Dead Redemption 2', price: 2700},
+  {title: 'Sekiro', price: 1500},
+  {title: 'Detroit become human', price: 500},
+  {title: 'Until dawn', price: 500},
+  {title: 'Final Fantasy XV', price: 700},
+])
